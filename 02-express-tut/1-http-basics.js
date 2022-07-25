1f8300cda181f28bd6a202d80ecd061f9eff478c
sahil6699 <sahil66199@gmail.com>
2022-07-25 16:49:17 +0530

    more on basic http server

--- 02-express-tut/1-http-basics.js
+++ 02-express-tut/1-http-basics.js
@@ -2,6 +2,12 @@ const http = require("http");
 
 //createServer method gets hit every time user hits the server
 const server = http.createServer((req, res) => {
+  //to know what route we are hitting we use the url method
+  console.log(req.url);
+
+  //to know what kind of request is coming
+  console.log(req.method);
+
   //meta data for our respone
   // status-code , {type-of-data we are sending}
   res.writeHead(200, { "content-type": "text/html" });
