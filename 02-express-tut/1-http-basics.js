439b94c8ed1f30e0a1036c1ee8b523a817d59623
sahil6699 <sahil66199@gmail.com>
2022-07-25 20:13:47 +0530

    serving a page with html and css using the http module

--- 02-express-tut/1-http-basics.js
+++ 02-express-tut/1-http-basics.js
@@ -1,12 +1,17 @@
 const http = require("http");
 
 const { readFileSync } = require("fs");
-const homePage = readFileSync("./index.html");
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 //createServer method gets hit every time user hits the server
 const server = http.createServer((req, res) => {
   const url = req.url;
+
   console.log(url);
+
   if (url === "/") {
     //home page
 
@@ -20,6 +25,21 @@ const server = http.createServer((req, res) => {
     //about page
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>About page</h1>");
+    res.end();
+  } else if (url === "/styles.css") {
+    //styles
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  } else if (url === "/logo.svg") {
+    //logo svg
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    //front end js
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
     res.end();
   } else {
     //404 page
