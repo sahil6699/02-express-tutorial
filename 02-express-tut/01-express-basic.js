51a880a340c2d18471710ef96aeb870a42976ba2
sahil6699 <sahil66199@gmail.com>
2022-07-25 20:38:33 +0530

    boiler plate code for expressjs

---
+++ 02-express-tut/01-express-basic.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server is listening on port ", 5000);
+});
+
+/*
+  app.get
+  app.post
+  app.delete
+  app.put
+  app.delete
+  app.all
+  app.listen
+  app.use
+*/
