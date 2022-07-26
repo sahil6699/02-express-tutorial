149b15feb17dedf3bd709b7b28d9db5b169f81bd
sahil6699 <sahil66199@gmail.com>
2022-07-26 16:16:42 +0530

    route parametes in express
    1. write :productId in the the parameter
    2. make a variable prodcutId and destructure the req.params for getting the value in the variable
    3. use the find method to get the product

--- 02-express-tut/04-params-query-strings.js
+++ 02-express-tut/04-params-query-strings.js
@@ -7,12 +7,31 @@ app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1><a href ='/api/products'>products</a>");
 });
 
+//for route api/product and only printing the name , id , image and not description
 app.get("/api/products", (req, res) => {
   const newProducts = products.map((product) => {
     const { id, name, image } = product;
     return { id, name, image };
   });
   res.send(newProducts);
+});
+
+//let say i want to print about the with id 1
+// req.params give the output of whatever is given after the route
+app.get("/api/products/:productId", (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  /* req.params
+This property is an object containing properties mapped to the named route “parameters”.
+For example, if you have the route /user/:name, then the “name” property is available as req.params.name. 
+This object defaults to {}. */
+
+  const { productId } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productId)
+  );
+  res.send(singleProduct);
 });
 
 app.listen(5000, () => {
