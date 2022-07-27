fc4fcf9fd8d8115ddb9ac19edd82a6520a9db722
sahil6699 <sahil66199@gmail.com>
2022-07-27 16:33:58 +0530

    middleware in express
    middleware are something which run after a request is taken and before a response is subbmitted it has acces to both the req and res object
    
    in a middleware we have to call the next method or we have to give a response to end the req, res cycle
    
    if we end the req, res cycle with a res then the next middleware for the route will not run and the respone we have subbmitted that will be runned for that route

--- 02-express-tut/05-middleware-basisc.js
+++ 02-express-tut/05-middleware-basisc.js
@@ -1,6 +1,23 @@
 const express = require("express");
-
 const app = express();
+
+//req => middleware => response
+function logger(req, res, next) {
+  const method = req.method;
+  const url = req.url;
+  const date = new Date().getFullYear();
+  console.log(method, url, date);
+  // next();
+  res.send("Tester op");
+}
+
+app.get("/", logger, (req, res) => {
+  res.send("Home Page");
+});
+
+app.get("/about", logger, (req, res) => {
+  res.send("About Page");
+});
 
 app.listen(5000, () => {
   console.log("server is listening on port ", 5000);
