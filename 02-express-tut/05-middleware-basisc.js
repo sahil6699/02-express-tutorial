bfdaf165e7b3134bbb51f942e1db9006cbd9a8e3
sahil6699 <sahil66199@gmail.com>
2022-07-28 12:21:25 +0530

    app.use in express
    1. app.use gets applied to all the requests below app.use if we don't give the request
    2. if we give  request then it gets applied to only the path which have the same base routes as we have given

--- 02-express-tut/05-middleware-basisc.js
+++ 02-express-tut/05-middleware-basisc.js
@@ -6,6 +6,13 @@ const app = express();
 //the middleware  we mention in app.use gets applied to all the routes - if it is placed at the top
 //the order matters - if we apply it to below a route then it would not be applied to the route above it would only be applied to all the routes which are below it
 //hence we keep the middleware which has to be applied on all the routes on the top
+// app.use(logger);
+
+//we can give the path to the app.use
+// aur app.use ke andar ke middleware sirf unn routes pr apply hoga jo iss mentioned routes ke baad ayenge
+// for eg: route:- /api , tuh middleware khali /api/productcs , /api/items/teddy
+// in jaise routes pr hi apply hoga , baki kisi route pr apply ni hoga
+app.use("/api", logger);
 
 app.get("/", (req, res) => {
   res.send("Home Page");
@@ -14,7 +21,6 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.send("About Page");
 });
-app.use(logger);
 
 app.get("/api/products", (req, res) => {
   res.send("Products");
@@ -22,6 +28,10 @@ app.get("/api/products", (req, res) => {
 
 app.get("/api/items", (req, res) => {
   res.send("Items");
+});
+
+app.get("/api/people", (req, res) => {
+  res.send("People");
 });
 
 app.listen(5000, () => {
