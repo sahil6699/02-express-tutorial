e27c1ed87d71e835b936f8fc8f38f7a7bd753e82
theScodes <ahujsushant01@gmail.com>
2022-08-01 16:44:29 +0530

    setting up router for people js

--- 02-express-tut/08-post-method-javascript.js
+++ 02-express-tut/08-post-method-javascript.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-let { people } = require("./data");
-
+const people = require("./routes/people");
 //static assets
 app.use(express.static("./methods-public"));
 //parse form data
@@ -10,29 +9,8 @@ app.use(express.urlencoded({ extended: false }));
 //parse json
 app.use(express.json());
 
-app.post("/api/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide the value" });
-  }
-  res.status(201).json({ success: true, person: name });
-});
-
-app.post("/api/postman/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide the name value" });
-  }
-  res.status(201).json({ success: true, data: [...people, name] });
-});
-
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+//set people router
+app.use("/api/people/", people);
 
 app.post("/login", (req, res) => {
   //using javascript to check if we entered a particular text or not
@@ -42,51 +20,6 @@ app.post("/login", (req, res) => {
   }
   console.log(req.body);
   res.status(401).send("Please enter the given credentials");
-});
-
-app.put("/api/people/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  // console.log(id, name);
-  // res.send("hello world");
-  const person = people.find((person) => person.id === Number(id));
-  console.log(person);
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `The person with ${id} doesn't exist` });
-  }
-
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-  res.status(200).json({ success: true, data: newPeople });
-});
-
-// app.delete("api/people/:id", (req, res) => {
-//   const person = people.find((person) => person.id === Number(req.params.id));
-//   if (!person) {
-//     return res
-//       .status(404)
-//       .json({ success: false, msg: `no person with ${id} ` });
-//   }
-// });
-app.delete("/api/people/:id", (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id));
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${req.params.id}` });
-  }
-  const newPeople = people.filter(
-    (person) => person.id !== Number(req.params.id)
-  );
-  return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
