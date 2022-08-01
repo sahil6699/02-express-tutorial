0faf683df53dff7e2be42e495544b078c4e0f02c
theScodes <ahujsushant01@gmail.com>
2022-08-01 17:44:39 +0530

    chaining the request on the same url using the router.route method
    1. use router.route("path").get or .post or any other reqest for this path

--- 02-express-tut/routes/people.js
+++ 02-express-tut/routes/people.js
@@ -9,14 +9,18 @@ const {
   deletePerson,
 } = require("../controllers/people");
 
-router.get("/", getPeople);
+//we can use this setup
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("/postman", createPersonPostman);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
 
-router.post("/", createPerson);
+//OR
+//chaining the callback methods for the same route
 
-router.post("/postman", createPersonPostman);
-
-router.put("/:id", updatePerson);
-
-router.delete("/:id", deletePerson);
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
